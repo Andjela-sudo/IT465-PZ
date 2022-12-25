b8532b2d6aa3405db4340487785c19f161b134b1
Andjela Bojic <abojic@propeler.io>
2022-12-25 19:19:53 +0100

    dispatcher

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+import { createTheme, CssBaseline, Stack, ThemeProvider } from '@mui/material';
 import Container from '@mui/material/Container';
-import { useEffect, useState } from 'react';
+import { useReducer } from 'react';
 import TabsDivider from './components/TabsDivider';
-import { stringEncryption } from './vernam_cipher';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 
 function App() {
 
-  const [key, setKey] = useState('')
-  const [shouldTrackMouse, setShouldTrackMouse] = useState(true)
-  const [message, setMessage] = useState('')
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'set_key': return { ...state, key: action.payload }
+      case 'set_should_track_mouse': return { ...state, shouldTrackMouse: action.payload }
+      case 'set_message': return { ...state, message: action.payload }
+      default: return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, {
+    key: '',
+    shouldTrackMouse: true,
+    message: '',
+  });
+
 
   // useEffect(()=>{},[key])
 
-
   const encryptTextMessage = () => {
-    setShouldTrackMouse(false)
-    setKey(key.slice(0, message.length))
+    dispatch({ type: "set_should_track_mouse", payload: false });
+    dispatch({ type: "set_key", payload: state.key.slice(0, state.message.length) });
 
     // enkriptovanje poruke
     // stringEncryption(message, key)
@@ -22,16 +40,32 @@ function App() {
     //cuvanje key u dokument
   }
 
-
-
   const handleMouseMove = (event) => {
-    setKey(key + (event.clientX + event.clientY))
+    dispatch({ type: "set_key", payload: state.key + (event.clientX + event.clientY) });
   }
 
   return (
-    <Container onMouseMove={shouldTrackMouse ? handleMouseMove : undefined} maxWidth="sm">
-      <TabsDivider message={message} setMessage={setMessage} encryptTextMessage={encryptTextMessage} />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container
+        sx={{
+          display: 'flex',
+          justifyContent: 'center'
+        }}>
+        <Stack
+          sx={{
+            direction: 'column',
+            justifyContent: 'center',
+            height: '100vh',
+          }}
+          onMouseMove={state.shouldTrackMouse ? handleMouseMove : undefined} maxWidth="sm">
+          <TabsDivider
+            state={state}
+            dispatch={dispatch}
+            encryptTextMessage={encryptTextMessage} />
+        </Stack>
+      </Container>
+    </ThemeProvider>
   );
 }
 
